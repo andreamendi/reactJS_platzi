6a4e01734996224312d43348b3ebbb03ac690706
AndreaMendi <andreamendi@icloud.com>
2019-10-07 23:18:53 -0500

    Se agrego la carpeta de assets y se instalaron los pluging de sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
-    output: {
+    output: { 
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
@@ -11,7 +12,7 @@ module.exports = {
         extensions: ['.js', '.jsx']
     },
     devServer: {
-        port: 9000
+        port: 3002
     },
     module: {
         rules: [
@@ -27,6 +28,16 @@ module.exports = {
                 use: {
                     loader: "html-loader"
                 }
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -34,6 +45,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'assets/[name].css'
         }),
     ]
 };
