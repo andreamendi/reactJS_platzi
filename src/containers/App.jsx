79d168c21927920b8fa94bba180493fb40eac1cb
AndreaMendi <andreamendi@icloud.com>
2019-10-09 23:37:08 -0500

    Hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,46 +1,48 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const App = () => {
 
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  const API = 'http://localhost:3000/initalState/';
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  const InitialState = useInitialState(API);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
 
-    <Categories title='Originales de Platfix '>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      {InitialState.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            {InitialState.mylist.map((item) => <CarouselItem key={item.id} {...item} />)}
+          </Carousel>
+        </Categories>
+      )}
 
-    <Footer />
-  </div>
-);
+      <Categories title='Tendencias'>
+        <Carousel>
+          {InitialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
 
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales de Platfix '>
+        <Carousel>
+          {InitialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
 export default App;
