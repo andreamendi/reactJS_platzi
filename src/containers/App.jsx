c8f3f07b9944dc2c7c6823924dc5a0a6f926d54b
AndreaMendi <andreamendi@icloud.com>
2019-10-09 21:23:09 -0500

    Estilos listos, webpack listo, y la fake API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -13,11 +13,26 @@ const App = () => (
     <Header />
     <Search />
 
-    <Categories>
+    <Categories title='Mi lista'>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Tendencias'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Originales de Platfix '>
+      <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
